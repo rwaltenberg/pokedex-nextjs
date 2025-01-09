33db4f9e9d7ecf5d76b647e48fef8d8dd069a2f7
Rodrigo Waltenberg <rwaltenberg@gmail.com>
2025-01-08 22:36:20 -0300

    simplify page structure

--- app/page.tsx
+++ app/page.tsx
@@ -9,14 +9,10 @@ export default function Home() {
   const { data } = useSuspenseQuery(GetPokemonListDocument)
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
-          {data.pokemon.map((pokemon) => (
-            <PokeCard key={pokemon.id} pokemon={pokemon} />
-          ))}
-        </div>
-      </main>
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 max-w-5xl mx-auto my-8">
+      {data.pokemon.map((pokemon) => (
+        <PokeCard key={pokemon.id} pokemon={pokemon} />
+      ))}
     </div>
   )
 }
