fd3c4969d9bd19fe43c455b7902c06a21d26797f
Rodrigo Waltenberg <rwaltenberg@gmail.com>
2025-01-09 23:35:43 -0300

    add virtualization for pokémon cards to reduce rendering overhead

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 "use client"
 
+import RenderIfVisible from "react-render-if-visible"
+
 import PokeCard from "@/components/PokeCard"
 import { usePokemonList } from "@/hooks/usePokemonList"
 
@@ -9,11 +11,14 @@ export default function Home() {
   return (
     <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 lg:gap-8 container lg:max-w-5xl mx-auto my-4 lg:my-8 px-4 lg:px-0">
       {data.map((pokemon, index) => (
-        <PokeCard
+        <RenderIfVisible
           key={pokemon.id}
-          pokemon={pokemon}
-          priorityImage={index < 20}
-        />
+          defaultHeight={325}
+          initialVisible={index < 20}
+          visibleOffset={2000}
+        >
+          <PokeCard pokemon={pokemon} priorityImage={index < 20} />
+        </RenderIfVisible>
       ))}
     </div>
   )
